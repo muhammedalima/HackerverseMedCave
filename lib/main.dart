e7841673328907da3217ca28a7cce97e850a08e6
Kali-05 <kalidasp80@gmail.com>
2025-01-11 23:51:34 +0530

    change in main page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       theme: AppTheme.theme,
-      home:  AmbulancescreenDriver(),
+      home:  SplashScreen(),
     );
   }
 }
